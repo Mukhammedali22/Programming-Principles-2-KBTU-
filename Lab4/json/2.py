668da6c4d4a5f533c26209e7488e15d7f637bdae
Mukhammedali22 <98638306+Mukhammedali22@users.noreply.github.com>
2022-03-02 19:01:13 +0600

    Add files via upload

---
+++ Lab4/json/2.py
@@ -0,0 +1,13 @@
+import json
+with open("sample-data.json") as jsonFile:
+    x = json.load(jsonFile)
+print(
+"""
+Interface Status
+================================================================================
+DN                                                 Description           Speed    MTU  
+-------------------------------------------------- --------------------  ------  ------
+"""
+)
+for i in x["imdata"]:
+    print('{DN:50}{Speed:>29}{MTU:>7}'.format(DN = i["l1PhysIf"]["attributes"]["dn"], Speed = i["l1PhysIf"]["attributes"]["speed"], MTU = i["l1PhysIf"]["attributes"]["mtu"]))
