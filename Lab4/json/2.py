433b62db2276a5c033f2cb87652781f9afe5bfbd
Mukhammedali22 <98638306+Mukhammedali22@users.noreply.github.com>
2022-03-02 19:03:44 +0600

    Update 2.py

--- Lab4/json/2.py
+++ Lab4/json/2.py
@@ -11,3 +11,4 @@ DN                                                 Description           Speed  
 )
 for i in x["imdata"]:
     print('{DN:50}{Speed:>29}{MTU:>7}'.format(DN = i["l1PhysIf"]["attributes"]["dn"], Speed = i["l1PhysIf"]["attributes"]["speed"], MTU = i["l1PhysIf"]["attributes"]["mtu"]))
+    # удлиняем нашу строку до заданной длины добавляя ей пробелов, а затем печатаем получившую новую строку
