fbfd26bcc384c3a7720b36b235fdcf6c8f1365b5
Mukhammedali22 <98638306+Mukhammedali22@users.noreply.github.com>
2022-03-13 16:29:54 +0600

    Update 1.py

--- Lab6/builtin-functions/1.py
+++ Lab6/builtin-functions/1.py
@@ -6,3 +6,13 @@ if all(my_list):
     print(sum)
 else:
     print("NO")
+
+'''  
+import math                                # second way
+a = [i for i in range(1, 10)]
+if all(a):
+    product = math.prod(a)
+    print(product)
+else:
+    print("Not iterable")
+'''
