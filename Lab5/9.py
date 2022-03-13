6d811be2f76cffda98d10e0ef33330620052b35b
Mukhammedali22 <98638306+Mukhammedali22@users.noreply.github.com>
2022-03-13 21:45:01 +0600

    Update 9.py

--- Lab5/9.py
+++ Lab5/9.py
@@ -1,5 +1,13 @@
 import re
 txt = "YouArePrettiesteGirlIHaveEverSeen, perfectNumber, BinToDec, camelCase, UpperCase"
+x = re.sub(r"([A-Z]|[a-z][a-z]*)([A-Z][a-z]*)", r"\1 \2 ", txt).strip()
+x = re.sub(r"( ,)+", r",", x)
+print(x)
+
+'''
+import re
+txt = "YouArePrettiesteGirlIHaveEverSeen, perfectNumber, BinToDec, camelCase, UpperCase"
 x = re.findall("[A-Z][a-z]*", txt)
 for i in x:
     print(i, end = " ")
+'''
