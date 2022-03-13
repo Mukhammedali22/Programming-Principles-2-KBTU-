14e42772a4c9b4c8d038388bab988adf1d33261d
Mukhammedali22 <98638306+Mukhammedali22@users.noreply.github.com>
2022-03-13 16:27:00 +0600

    Update 1.py

--- Lab6/builtin-functions/1.py
+++ Lab6/builtin-functions/1.py
@@ -1,8 +1,8 @@
 my_list = [i for i in range(1, 10)]
 if all(my_list):
-    sum = 1
+    product = 1
     for i in my_list:
-        sum *= i
+        product *= i
     print(sum)
 else:
     print("NO")
