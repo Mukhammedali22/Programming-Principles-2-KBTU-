ffbbc1e8245492db30db9cae4701a76b5f31710c
Mukhammedali22 <98638306+Mukhammedali22@users.noreply.github.com>
2022-03-13 21:38:02 +0600

    Update 10.py

--- Lab5/10.py
+++ Lab5/10.py
@@ -1,5 +1,13 @@
 import re
 #txt = input()
+txt = "YouArePrettiestGirlIHaveEverSeen"
+x = re.sub(r"([A-Z][a-z]*)([A-Z][a-z]*)", r"\1 \2 ", txt).strip()
+x = re.sub(" ", "_", x).lower()
+print(x)
+
+'''
+import re
+#txt = input()
 txt = "youArePrettiestGirlIHaveEverSeen"
 x = re.findall("^[a-z]+", txt) + re.findall("[A-Z][a-z]*", txt)
 #re.findall("^[a-z]+", txt) чтобы получить первое слово начинающийся с маленькой буквы это "you"
@@ -9,4 +17,5 @@ for i in x:
     if i[0].isupper():
         snake_case += '_'
     snake_case += i.lower()
-print(snake_case)    
+print(snake_case) 
+'''
