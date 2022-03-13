391a59dfecd9c2df61b16983e182e10321b85520
Mukhammedali22 <98638306+Mukhammedali22@users.noreply.github.com>
2022-03-13 21:51:42 +0600

    Update 8.py

--- Lab5/8.py
+++ Lab5/8.py
@@ -1,5 +1,12 @@
 import re
 txt = "YouArePrettiestGirlIHaveEverSeen"
+x = re.sub(r"([A-Z][a-z]*)([A-Z][a-z]*)", r"\1 \2 ", txt).strip()
+print(x)
+
+'''
+import re
+txt = "YouArePrettiestGirlIHaveEverSeen"
 x = re.findall("[A-Z][a-z]*", txt)
 for i in x:
     print(i, end = " ")
+'''
