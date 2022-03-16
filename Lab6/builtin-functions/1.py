b27285e3329d766c2ddd4373a8c004b7c7d08b01
Mukhammedali22 <98638306+Mukhammedali22@users.noreply.github.com>
2022-03-16 23:18:58 +0600

    Update 1.py

--- Lab6/builtin-functions/1.py
+++ Lab6/builtin-functions/1.py
@@ -1,4 +1,5 @@
-my_list = [i for i in range(1, 10)]
+#my_list = [i for i in range(1, 10)]
+my_list = list(map(int, input().split()))
 product = 1
 for i in range(len(my_list)):
     product *= my_list[i]
@@ -7,6 +8,7 @@ print(product)
 ''' 
 import math                                # second way
 a = [i for i in range(1, 10)]
+#a = list(map(int, input().split()))
 product = math.prod(a)
 print(product)
 '''
