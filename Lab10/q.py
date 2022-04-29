ba4fae8f985a15f9ac5ee5344b0e6604071c25c1
Mukhammedali22 <98638306+Mukhammedali22@users.noreply.github.com>
2022-04-29 21:51:39 +0600

    Delete Lab10 directory

--- Lab10/q.py
+++
@@ -1,7 +0,0 @@
-import psycopg2
-
-conn = psycopg2.connect(dbname='suppliers', user='postgres', password='123qwe123', host='localhost')
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM parts")
-results = cursor.fetchone()
-print(results)
